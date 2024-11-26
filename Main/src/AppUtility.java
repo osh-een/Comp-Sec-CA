53a1de286bba132eaaf62f3913e39b95299e4197
osh-een <oisinbell09@gmail.com>
2024-11-26 11:02:24 +0000

    added aes method

--- Main/src/AppUtility.java
+++ Main/src/AppUtility.java
@@ -1,4 +1,8 @@
+import java.security.*;
 import java.util.*;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 
 public class AppUtility {
     static Scanner kb = new Scanner(System.in);
@@ -14,5 +18,16 @@ public class AppUtility {
                 kb.nextLine();
             }
         }
+    }
+
+    public static String encrypt(String algorithm, String input, SecretKey key, IvParameterSpec iv)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        byte[] cipherText = cipher.doFinal(input.getBytes());
+        return Base64.getEncoder().encodeToString(cipherText);
     }
 }
