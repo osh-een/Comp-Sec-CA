8bbbf764b51afe342842bab971aa3c9bfead3439
osh-een <oisinbell09@gmail.com>
2024-11-26 09:28:33 +0000

    initial

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class App {
+
+    static Scanner kb = new Scanner(System.in);
+    // 1. Encrypt a File (Task 2)
+    // 2. Decrypt a File (Task 3)
+    // 3. Quit the application
+
     public static void main(String[] args) {
+        while (true) {
+            int n = kb.nextInt();
+            // int num = getNumber();
+            if (n == 1) {
+
+            }
+            // System.out.println(s);
+        }
 
     }
-}
+
+    public static int getNumber(){
+        int num;
+       try{
+       num = kb.nextInt();
+
+    catch(InputMismatchException e){
+            kb.nextLine();
+        
+        }
+}}
