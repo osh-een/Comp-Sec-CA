bb8cee59eade499daa51fc36f783adfef1ffb61d
osh-een <oisinbell09@gmail.com>
2024-11-26 10:23:40 +0000

    added appUtility

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class App {
+
+    static Scanner kb = new Scanner(System.in);
+    // 1. Encrypt a File (Task 2)
+    // 2. Decrypt a File (Task 3)
+    // 3. Quit the application
+
     public static void main(String[] args) {
 
+        while (true) {
+            System.out.println("Please select an option: \n1. Encrypt File \n2.Decrypt File \n3. Quit");
+            int num = AppUtility.getNumber();
+            if (num == 1) {
+                System.out.println("Encrypting...");
+            } else if (num == 2) {
+                System.out.println("Decrypting...");
+            } else if (num == 3) {
+                System.out.println("GoodBye!");
+                break;
+            }
+        }
     }
+
+    
 }
