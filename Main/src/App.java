a45dcefa76c88a7f6b483dffcf9f63c9a7e7c2c9
osh-een <oisinbell09@gmail.com>
2024-11-29 22:59:24 +0000

    added code from referenced website

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import javax.crypto.SecretKey;
+
 public class App {
 
     static Scanner kb = new Scanner(System.in);
@@ -8,11 +10,12 @@ public class App {
     // 3. Quit the application
 
     public static void main(String[] args) {
-
+        String algorithm = "AES/CBC/PKCS5Padding", input = "Hello World";
         while (true) {
             System.out.println("Please select an option: \n1. Encrypt File \n2.Decrypt File \n3. Quit");
             int num = AppUtility.getNumber();
             if (num == 1) {
+
                 System.out.println("Encrypting...");
             } else if (num == 2) {
                 System.out.println("Decrypting...");
--- Main/src/AppUtility.java
+++ Main/src/AppUtility.java
@@ -1,8 +1,8 @@
-import java.security.*;
 import java.util.*;
-
+import java.io.*;
+import java.security.*;
 import javax.crypto.*;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.*;
 
 public class AppUtility {
     static Scanner kb = new Scanner(System.in);
@@ -29,5 +29,98 @@ public class AppUtility {
         cipher.init(Cipher.ENCRYPT_MODE, key, iv);
         byte[] cipherText = cipher.doFinal(input.getBytes());
         return Base64.getEncoder().encodeToString(cipherText);
+    }
+
+    public static void encryptFile(String algorithm, SecretKey key, IvParameterSpec iv,
+            File inputFile, File outputFile) throws IOException, NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] output = cipher.update(buffer, 0, bytesRead);
+            if (output != null) {
+                outputStream.write(output);
+            }
+        }
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+
+    public static void decryptFile(String algorithm, SecretKey key, IvParameterSpec iv,
+            File inputFile, File outputFile) throws IOException,
+            NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] output = cipher.update(buffer, 0, bytesRead);
+            if (output != null) {
+                outputStream.write(output);
+            }
+        }
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+
+    void givenFile_whenEncrypt_thenSuccess()
+            throws NoSuchAlgorithmException, IOException, IllegalBlockSizeException,
+            InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException,
+            NoSuchPaddingException {
+
+        SecretKey key = generateKey(128);
+        String algorithm = "AES/CBC/PKCS5Padding";
+        IvParameterSpec ivParameterSpec = generateIv();
+        Resource resource = new ClassPathResource("inputFile/baeldung.txt");
+        File inputFile = resource.getFile();
+        File encryptedFile = new File("classpath:baeldung.encrypted");
+        File decryptedFile = new File("document.decrypted");
+        encryptFile(algorithm, key, ivParameterSpec, inputFile, encryptedFile);
+        decryptFile(
+                algorithm, key, ivParameterSpec, encryptedFile, decryptedFile);
+        if ((inputFile).equals(decryptedFile)) {
+
+        }
+    }
+
+    public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(n);
+        SecretKey key = keyGenerator.generateKey();
+        return key;
+    }
+
+    public static SecretKey getKeyFromPassword(String password, String salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+        SecretKey secret = new SecretKeySpec(factory.generateSecret(spec)
+                .getEncoded(), "AES");
+        return secret;
+    }
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
     }
 }
